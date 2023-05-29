33307ee3b11bbe4f9da8cff2a0bcb3c4fd132d77
BintangMy <d2maw5@gmail.com>
2023-05-29 13:12:54 +0700

    final server webGPT

--- server/controllers/controllerOpenAi.js
+++ server/controllers/controllerOpenAi.js
@@ -8,7 +8,6 @@ class controllerOpenAi {
   static async summary(req, res, next) {
     try {
       const { text } = req.body;
-     
       const { data } = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: `Summarize this \n${text}`,
@@ -16,20 +15,17 @@ class controllerOpenAi {
         temperature: 0.5,
       });
 
-      if (!data) throw Error;
+      if (!data) throw { name: "notFound" };
       res.status(200).json(data?.choices[0].text);
     } catch (error) {
-      console.log(error, 'ini errorrrr');
-      return res.status(404).json({
-        message: error.message,
-      });
+      console.log(error);
+      next(error);
     }
   }
 
   static async paragraph(req, res, next) {
     try {
       const { text } = req.body;
-      console.log(text, "===============================>>>>>>>>");
       const { data } = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: `write a detail paragraph about \n${text}`,
@@ -37,44 +33,38 @@ class controllerOpenAi {
         temperature: 0.5,
       });
 
-      if (!data) throw { name: "notFound" }
-      console.log(data, 'ini result dari controller.......')
+      if (!data) throw { name: "notFound" };
       res.status(200).json(data?.choices[0].text);
     } catch (error) {
-      console.log(error, 'ini error controller....')
-      next(error)
+      console.log(error);
+      next(error);
     }
   }
 
   static async chatbot(req, res, next) {
     try {
       const { text } = req.body;
-      console.log(text);
       const { data } = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `Answer question similar to how bintang from star war would.
+        prompt: `Answer question similar to how ultra from star war would.
       Me: 'what is your name?'
-      bintang: 'bintang is my name'
+      Ultra: 'Ultra is my name'
       Me: ${text}`,
         max_tokens: 300,
         temperature: 0.7,
       });
 
-      if (!data) throw Error;
+      if (!data) throw { name: "notFound" };
       res.status(200).json(data?.choices[0].text);
     } catch (error) {
       console.log(error);
-      return res.status(404).json({
-        message: error.message,
-      });
+      next(error);
     }
   }
 
   static async jsCoding(req, res, next) {
     try {
       const { text } = req.body;
-      console.log(text);
-
       const { data } = await openai.createCompletion({
         model: "text-davinci-002",
         prompt: `/* convert these instruction into javascript code \n${text}`,
@@ -82,34 +72,28 @@ class controllerOpenAi {
         temperature: 0.25,
       });
 
-      if (!data) throw Error;
+      if (!data) throw { name: "notFound" };
       res.status(200).json(data?.choices[0].text);
     } catch (error) {
       console.log(error);
-      return res.status(404).json({
-        message: error.message,
-      });
+      next(error);
     }
   }
 
   static async imageGenerator(req, res, next) {
     try {
       const { text } = req.body;
-      console.log(text);
-
       const { data } = await openai.createImage({
         prompt: `generate a scifi image of ${text}`,
         n: 1,
         size: "512x512",
       });
 
-      if (!data) throw Error;
+      if (!data) throw { name: "notFound" };
       res.status(200).json(data?.data[0].url);
     } catch (error) {
       console.log(error);
-      return res.status(404).json({
-        message: error.message,
-      });
+      next(error);
     }
   }
 }
